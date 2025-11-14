dd02149637b7655d835b3dab56f99384765d525b
Ryley Turner <losycresent@gmail.com>
2025-11-13 22:31:23 -0600

    changes

--- frontend/src/home/home-vm.ts
+++ frontend/src/home/home-vm.ts
@@ -1,17 +1,22 @@
-// Previous Games
 type GameResult = {
     duration: number;
     winner: number;
 }
 
-// Open Games
 type OpenGame = {
     game_id: string;
     
 }
 
+type Friend = {
+    username: string;
+    status: number;
+    profile_picture: Object;
+}
+
 export type HomeViewModel = {
-    user_name: string;
+    username: string;
     previous_games: GameResult[];
     open_games: OpenGame[];
+    friends: Friend[];
 }
