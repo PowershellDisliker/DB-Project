33f4a66ace965ceaa6651e2da2c5b0fe1a9e0972
Ryley Turner <losycresent@gmail.com>
2025-11-13 22:54:19 -0600

    added comps

---
+++ frontend/src/components/loading/loading.tsx
@@ -0,0 +1,11 @@
+import loadingStyles from "./loading.module.css"
+
+function LoadingIcon() {
+
+
+    return (
+        <div className={loadingStyles.spinner}>
+
+        </div>
+    )
+}
--- frontend/src/home/home.tsx
+++ frontend/src/home/home.tsx
@@ -1,21 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
 import globalStyles from "../global.module.css";
 import homeStyles from "./home.module.css";
+import type { HomeViewModel } from "./home-vm";
 
 function Home() {
+    const [viewModel, setViewModel] = useState<HomeViewModel>();
 
     return (
         <div className={`${globalStyles.row} ${globalStyles.globalCenter}`}>
+
             <div className={`${globalStyles.roundedContainer}`}>
                 <h1>Friends List</h1>
+                {viewModel?.friends && <LoadingIcon/>}
+                <ul>
+                    {viewModel?.friends.map((value) => {
+                        return (
+                            <Friend props={value}/>
+                        )
+                    })}
+                </ul>
             </div>
+
             <div className={`${globalStyles.column} ${globalStyles.roundedContainer}`}>
                 <h1>Hello User!</h1>
                 <h3>Open Server / Lobby List</h3>
+                {viewModel?.open_games && <LoadingIcon/>}
+                <ul>
+                    {viewModel?.open_games.map((value) => {
+                        return (
+                            <OpenGame props={value} />
+                        )
+                    })}
+                </ul>
             </div>
+
             <div className={`${globalStyles.roundedContainer}`}>
                 <h1>Player Stats</h1>
+                {viewModel?.previous_games && <LoadingIcon/>}
+                <ul>
+                    {viewModel?.previous_games.map((value) => {
+                        return (
+                            <PreviousGame props={value} />
+                        )
+                    })}
+                </ul>
             </div>
+
         </div>
     )
 }
