55c74174f7b5e9fc93d1b08a7d41bc468ba5a06a
Ryley Turner <losycresent@gmail.com>
2025-11-29 15:42:13 -0600

    changes

--- frontend/src/components/game-canvas/game-state.ts
+++
@@ -1,4 +0,0 @@
-export type GameState = {
-    board: number[][];
-    current_player: string;
-}
--- frontend/src/components/game-canvas/game.tsx
+++ frontend/src/components/game-canvas/game.tsx
@@ -1,10 +1,15 @@
 import {useEffect, useRef, useState} from 'react';
-import { type GameState } from './game-state';
-import { type GameViewModel } from './game-vm';
+import { type SlowState, type RealTimeState } from './game-vm';
 
 const HorizontalAspectRatio: number = 16;
 const VerticalAspectRatio: number = 9;
-const scale: number = 10;
+const scale: number = 60;
+const margin: number = 100;
+
+const ROWS: number = 6;
+const COLS: number = 7;
+
+const PIECE_RADIUS: number = 60;
 
 interface GameCanvasProps {
     beginning_board_state: number[][],
@@ -13,23 +18,70 @@ interface GameCanvasProps {
 
 function GameCanvas({beginning_board_state, current_player}: GameCanvasProps) {
 
-    const canvasRef = useRef(null);
-    const ws = useRef(null);
+    // setup state
+    const [viewModel, setViewModel] = useState<SlowState>();
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const ws = useRef<WebSocket>(null);
+    const RealTimeState = useRef<RealTimeState>(null);
+    
+    // get user input and send it to backend
+    const canvasClickHandler = (e: React.MouseEvent<HTMLCanvasElement>) => {
+        // If it isn't our turn.
+        if (!viewModel?.current_player) return;
 
-    const [viewModel, setViewModel] = useState<GameViewModel>();
+        const x = e.clientX;
+        const y = e.clientY;
 
-    const gameState = useRef<GameState>({
-        board: beginning_board_state,
-        current_player: current_player,
-    });
+        // clicked out of bounds
+        if (x < margin / 2 || x > margin / 2 + (e.currentTarget.width - margin)) return;
 
+        const ColumnWidth = (e.currentTarget.width - margin) / COLS;
+        const piece_col = (x - (margin / 2)) / ColumnWidth;
+
+        
+    }
+
+    // animation effect
     useEffect(() => {
+        const canvas = canvasRef.current;
+        if (!canvas) return;
 
+        const GC = canvas.getContext('2d');
+        if (!GC) return;
+
+        const ImageMask = new Image();
+        ImageMask.src = "../../../public/board-mask.png";
+
+        const drawFrame = () => {
+            // clear the canvas
+            GC.clearRect(0, 0, canvas.width, canvas.height);
+
+            // Draw all the pieces
+            pieces.current.forEach((piece: Piece) => {
+                GC.beginPath();
+                GC.arc(piece.x_pos, piece.y_pos, 30, 0, Math.PI * 2);
+                GC.fillStyle = "black";
+                GC.fill();
+            })
+
+
+            GC.drawImage(ImageMask, 0, 0);
+            requestAnimationFrame(drawFrame);
+        }
+
+        requestAnimationFrame(drawFrame);
     }, [])
 
-    return (
-        <canvas width={HorizontalAspectRatio * scale} height={VerticalAspectRatio * scale} ref={canvasRef}/>
-    )
+    // websocket effect
+    useEffect(() => {
+        ws.current = new WebSocket("");
+
+        return () => {
+            ws.current?.close();
+        }
+    }, [])
+
+    return <canvas width={HorizontalAspectRatio * scale} height={VerticalAspectRatio * scale} ref={canvasRef} onClick={canvasClickHandler}/>
 }
 
 export default GameCanvas;
