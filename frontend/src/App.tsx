c7646fbc4d984aa9dc7644dd6c9a13b3e005d7e9
Ryley Turner <losycresent@gmail.com>
2025-12-01 09:59:21 -0600

    merged merge

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from "./login";
 import { Home } from "./home";
 import { Game } from "./game";
-import * as dotenv from 'dotenv';
 
 interface Config {
   BACKEND_URL: string
