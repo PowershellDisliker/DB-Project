19245449ffe6f761b26adb997353f5b4276bf857
Ryley Turner <losycresent@gmail.com>
2025-12-03 23:24:21 -0600

    open games show usernames of players who opened them

--- frontend/src/components/open-game/open-game.tsx
+++ frontend/src/components/open-game/open-game.tsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import type { OpenGame } from '../../dto/opengame';
 import globalStyles from '../../global.module.css';
+import { getPublicUser } from '../../api';
+import { AuthContext, ConfigContext } from '../../context';
+import type { GetPublicUserResponse } from '../../dto';
 
 function OpenGameComp({game_id, user_1_id}: OpenGame) {
 
+    const [username, setUsername] = useState<string | null>(null);
     const navigator = useNavigate();
+    const config = useContext(ConfigContext);
+    const auth = useContext(AuthContext);
 
     const joinGame = () => {
         navigator(`/game?game_id=${game_id}`);
     }
 
+    useEffect(() => {const inner = async () => {
+        if (!auth.token) return;
+        const user: GetPublicUserResponse = await getPublicUser(config.BACKEND_URL, auth.token, user_1_id);
+        setUsername(user.username);
+    }
+
+    inner();
+    }, [])
+
     return (
         <div className={globalStyles.roundedContainer}>
             <button onClick={joinGame}>
-                <p>Player: {user_1_id}</p>
+                <p>Player: {username}</p>
             </button>
         </div>
     )
--- frontend/src/routes/game/game.tsx
+++ frontend/src/routes/game/game.tsx
@@ -1,15 +1,24 @@
 import React from "react";
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, useNavigate } from "react-router-dom";
 import {GameCanvas} from '../../components/game-canvas';
 import globalStyles from "../../global.module.css";
 
 function Game() {
 
     const [searchParams, setSearchParams] = useSearchParams();
+    const navigate = useNavigate();
+
+    const exitHandler = () => {
+        navigate("/home");
+    }
 
     return (
-        <div className={`${globalStyles.centerContainer}`}>
-            <GameCanvas game_id={searchParams.get("game_id")}/>
+        <div>
+            <button onClick={exitHandler}>Exit</button>
+
+            <div className={`${globalStyles.centerContainer} ${globalStyles.roundedContainer}`}>
+                <GameCanvas game_id={searchParams.get("game_id")}/>
+            </div>
         </div>
     )
 }
