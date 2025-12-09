81dae23e58aa724832bb53b553d72b7aebb009e6
Kdobs <kdobs1222@gmail.com>
2025-12-09 15:52:16 -0600

    win con 1.0

--- backend/game/game.py
+++ backend/game/game.py
@@ -1,102 +1,231 @@
-import json
+from typing import Tuple
+from dto import BoardState, DropPieceResponse
+import uuid
 
-BOARD_COLUMNS: int = 7
-BOARD_ROWS: int = 6
+# Holup
+ROW_COUNT: int = 6
+COL_COUNT: int = 7
 
-class gameState:
-    # object for sending data to the visulaizer in the front end
-    # needs the board, player turn, if the game is running, who won
-    board: ConnectFourBoard
-    winner = -1 # -1 if game is still going
-    turn = 0 # start wiht player 1
-
-    def __init__(self, CurrentBoard):
-        self.board = CurrentBoard
-
-    def updateTurn():
-        # swap to the next turn
-        if(turn == 0): 
-            turn = 1    
-        else:
-            turn = 0
-
-    def setWinner(self, outcome: int):
-        # set the winner to correct player
-        self.winner = outcome
-    
-    def toJson(self):
-        # convert to a json serialization to send to the front end
-        return json.dumps(self)
-    
-    def update(self, jsonString : str):
-        # update the game state based on the json input string
-        jsonDict = json.loads(jsonString) # convert the json string into a dictionary
-        self.winner = jsonDict["winner"]
-        self.turn = jsonDict["turn"]
-        self.board = jsonDict["board"]
-
-class Piece:
-    owner_number: int | None
-    def __init__(self, player: int):
-        self.owner_number = player
-        
 class ConnectFourBoard:
-    # board object that holds the possitions of the chips
-    player_1_id: int
-    player_2_id: int
-    positions: list[list[Piece]] = [[None for _ in range(BOARD_COLUMNS)] for _ in range(BOARD_ROWS)] # should fill the matrix with None until a piece is dropped
+    def __init__(self, user_1_id: uuid.UUID) -> None:
+        print(user_1_id)
+        self.user_1_id: uuid.UUID | None = user_1_id
+        self.user_2_id: uuid.UUID | None = None
 
-    
-    def __init__(self, p1_id: int, p2_id: int) -> None:
-        self.player_1_id = p1_id
-        self.player_2_id = p2_id
+        self.active_player: uuid.UUID = user_1_id
+        self.winner_id: uuid.UUID | None = None
+
+        self.positions: list[uuid.UUID | None] = [None for _ in range(COL_COUNT * ROW_COUNT)]
 
 
-    def drop_piece(self, piece_owner: int, column: int) -> bool:
+    def register_player(self, user_id: uuid.UUID) -> bool:
+        if self.user_1_id == user_id or self.user_2_id == user_id:
+            return False
+
+        if self.user_1_id is None:
+            self.user_1_id = user_id
+
+            if self.active_player is None:
+                self.active_player = user_id
+            return True
+
+        if self.user_2_id is None:
+            self.user_2_id = user_id
+            return True
+        return False
+
+
+    def drop_piece(self, piece_owner: uuid.UUID, col: int) -> DropPieceResponse:
         """
         Attempts to drop a connect 4 piece into the board at a specific column.
 
-        Returns true if possible and successful, false otherwise.
+        Returns true if possible and successful, false otherwise, and the UUID of the winner if there is one.
         """
-        for i in self.positions: # loop through the lists in position
-            if(i[column] != None):
-                #should be the first avalible position 
-                i[column] = Piece(piece_owner) # create a new piece
-        winner = self.check_for_winner()
-        if(winner != -1):
-            if(winner == 1): # player 1 wins
-                # need to decide how it will send out who the winner is·
-                pass
-            if(winner == 0): # player 0 wins
-                pass
+        # Only allow if both players are present
+        if self.user_1_id is None or self.user_2_id is None:
+            return DropPieceResponse(
+                success=False
+            )
+
+        # If the player placing the piece isn't currently in the game
+        if piece_owner != self.user_1_id and piece_owner != self.user_2_id:
+            return DropPieceResponse(
+                success=False
+            )
+
+        # If we're outside of the bounds
+        if col < 0 or col >= COL_COUNT:
+            return DropPieceResponse(
+                success=False
+            )
+
+        # If the active player isn't the piece_owner
+        if self.active_player != piece_owner:
+            return DropPieceResponse(
+                success=False
+            )
+
+        last_available_row = None
+        
+        # Get the available row
+        for row in reversed(range(ROW_COUNT)):
+            if self.positions[self.__get_index(row, col)] is None:
+                last_available_row = row
+                break
+
+        # Return if column is full
+        if last_available_row is None:
+            return DropPieceResponse(
+                success=False
+            )
+
+        # Place the piece
+        new_piece_index: int = self.__get_index(last_available_row, col)
+        self.positions[new_piece_index] = piece_owner
+
+        # Check for winner and return
+        winner = self.__check_for_winner(new_piece_index)
+
+        if winner is None:
+            self.active_player = self.user_1_id if piece_owner != self.user_1_id else self.user_2_id
+            return DropPieceResponse(
+                success=True,
+                coords=(last_available_row, col),
+                next_active_player_id=self.active_player
+            )
+        self.winner_id = winner
+        return DropPieceResponse(
+            success=True,
+            winner_id=winner,
+            coords=(last_available_row, col),
+            next_active_player_id=self.active_player
+        )
 
 
-        if column < 0 or column >= BOARD_COLUMNS:
-            return False
+    def get_board_state(self) -> BoardState:
+        return BoardState(
+            user_1_id=self.user_1_id,
+            user_2_id=self.user_2_id,
+            positions=self.positions,
+            active_player=self.active_player,
+            winner_id=self.winner_id
+        )
 
 
-    def check_for_winner(self) -> int:
+    def get_players(self) -> Tuple[uuid.UUID | None, uuid.UUID | None]:
+        return (self.user_1_id, self.user_2_id)
+
+
+    def get_active_player(self) -> uuid.UUID | None:
+        return self.active_player
+
+    def deregister_player(self, user_id: uuid.UUID) -> None:
+        if self.user_1_id == user_id:
+            self.user_1_id = None
+
+        elif self.user_2_id == user_id:
+            self.user_2_id = None
+
+
+    def __get_index(self, row: int, col: int) -> int:
+        return row * COL_COUNT + col
+
+
+    def __check_for_winner(self, recent_piece_index: int) -> uuid.UUID | None:
         """
-        Checks the board to see if there is a winner, returns the winner's player number
-        if there is a winner or -1 if there is not currently a winner.
+        returns winner's uuid if there is a winner, or None if there isn't
+
+        check the 8 piece around the last played piece then if there is one by the same user check the next two 
+        the board is held in posititons and it is just a linear list so each new row will be after 7 pieces or indieces
+        
+        the first col will be index%7 and the last col will be index%6
         """
-        #idea for checking the winner
-        """dfs for traversing the matrix like a graph
-        would need to keep track of the direction the the connection is made could prolly get it done with indexes"""
+        def check_col() -> uuid.UUID | None:
+            # check the col or left and right
+            right = 0
+            left = 0
+            for i in range(3):
+                if((recent_piece_index + 3) % 7 <= 3): # possible on the right
+                    if(self.positions[recent_piece_index] == self.positions[recent_piece_index + i]): # check right
+                        right = right + 1
+                if((recent_piece_index - 3) % 7 >= 3): # possible on the left
+                    if(self.positions[recent_piece_index] == self.positions[recent_piece_index - i]): # check left
+                        left = left + 1
+            if(right == 4):
+                return self.positions[recent_piece_index]
+            if(left == 4):
+                return self.positions[recent_piece_index]
+            if(right < 4 & left < 4):
+                return None
+            
+        def check_row() -> uuid.UUID | None:
+            # check the up or down and right
+            up = 0
+            down = 0
+            for i in range(3):
+                if(recent_piece_index // 7 >= 3): # up is possible
+                    if(self.positions[recent_piece_index] == self.positions[recent_piece_index + (i * 7)]): # check down
+                        up = up + 1
+                if(recent_piece_index // 7 <= 2): # down is possible
+                    if(self.positions[recent_piece_index] == self.positions[recent_piece_index - (i * 7)]): # check up
+                        down = down + 1
+            if(up == 4):
+                return self.positions[recent_piece_index]
+            if(down == 4):
+                return self.positions[recent_piece_index]
+            if(up < 4 & down < 4):
+                return None
+            
+        def check_dia() -> uuid.UUID | None:
+            # check the diagonals
+            upperleft = 0
+            upperright = 0
+            lowerleft = 0
+            lowerright = 0
+            for i in range(3):
+                if(recent_piece_index // 7 >= 3): # upper diagonals
+                    # now we check if there is enough room on the sides
+                    if((recent_piece_index + 3) % 7 <= 3): # check right
+                        if(self.positions[recent_piece_index] == self.positions[recent_piece_index + 1 - 7]):
+                            upperright = upperright + 1
+                    if((recent_piece_index - 3) % 7 <= 3): # check left
+                        if(self.positions[recent_piece_index] == self.positions[recent_piece_index - 1 - 7]):
+                            upperleft = upperleft + 1
+                if(recent_piece_index // 7 <= 2): # lower diagonals
+                    if((recent_piece_index + 3) % 7 <= 3): # check right
+                        if(self.positions[recent_piece_index] == self.positions[recent_piece_index + 1 + 7]):
+                            lowerright = lowerright + 1
+                    if((recent_piece_index - 3) % 7 <= 3): # check left
+                        if(self.positions[recent_piece_index] == self. positions[recent_piece_index -1 + 7]):
+                            lowerleft = lowerleft + 1            
 
-        return -1
-    
-    def dfsWinner(positions: list[list[Piece]], moveCount: int, lastMove:int):
-        # should return true if a winner is found
-        # want to start from the last move so we c
-        return 
+        col_winner = check_col()
+        row_winner = check_row()
+        dia_winner = check_dia()
 
-    def print_board(self) -> None:
+        if col_winner:
+            return col_winner
+
+        if row_winner:
+            return row_winner
+        
+        if dia_winner:
+            return dia_winner
+        return None
+
+
+    def __print_board(self) -> None:
         """
         low-level visual representation of the current board state for debugging
         """
-        for row in self.positions:
-            for value in row:
-                print(value.owner_number, end=" ")
+
+        for row in range(ROW_COUNT):
+            for col in range(COL_COUNT):
+                val = self.positions[self.__get_index(row, col)]
+
+                if val is None:
+                    print("0", end=" ")
+                else:
+                    print("1" if val == self.user_1_id else "2", end=" ")
             print()
         
