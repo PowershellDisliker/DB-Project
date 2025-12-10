05f6406a3fcf33fa8825945375174506d0fbd9ac
Ryley Turner <losycresent@gmail.com>
2025-12-09 21:15:32 -0600

    changes

--- frontend/src/routes/messages/messages.tsx
+++ frontend/src/routes/messages/messages.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { type GetMessageResponse, type PostMessageRequest } from '../../dto';
 import { MessageComp } from '../../components/message';
 import { ConfigContext } from '../../context';
@@ -6,10 +6,14 @@ import { getMessages } from '../../api';
 import { useSearchParams } from 'react-router-dom';
 import { postUserMessage } from '../../api';
 import { useCookies } from 'react-cookie';
+import globalStyles from '../../global.module.css';
 
 function Messages() {
     const config = useContext(ConfigContext);
     const [cookies] = useCookies(['jwt', 'id']);
+
+    const [reload, setReload] = useState<boolean>(false);
+    const inputRef = useRef<HTMLInputElement>(null);
 
     const [searchParams] = useSearchParams();
     const outbound_user_id: string | null = searchParams.get("user");
@@ -32,23 +36,25 @@ function Messages() {
             await compGetMessages();
         }
         inner();
-    }, [])
+    }, [reload])
 
     const sendHandler = async () => {
         await postUserMessage(config.BACKEND_URL, cookies.jwt, {
             recipient_id: outbound_user_id,
             message: messageInput,
-        } as PostMessageRequest)
+        } as PostMessageRequest);
+        inputRef.current!.value = "";
+        setReload(true);
     }
 
     return (
-        <div>
+        <div className={globalStyles.center}>
             <ul>
                 {messages?.messages?.map((value) => 
                     <MessageComp sender_id={value.sender_id} recipient_id={value.recipient_id} time_stamp={value.time_stamp} message={value.message} message_id={value.message_id} key={value.message_id}/>
                 )}
             </ul>
-            <input type="text" onChange={(event) => setMessageInput(event.target.value)} />
+            <input type="text" onChange={(event) => setMessageInput(event.target.value)} ref={inputRef}/>
             <button onClick={sendHandler}>Send</button>
         </div>
     )
